dba6e935f0fdc7b9c932989e22287386181d1a5d
Ande Caleb <andaeiii@aol.com>
2021-08-31 15:24:59 +0100

    protecting the routes and enabling the flask-user manager

--- app.py
+++ app.py
@@ -6,8 +6,40 @@ from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter
 
 app = Flask(__name__)
 
-dir = os.path.join(os.getcwd(), 'myfile.db')
-
 
 app.config['SECRET_KEY'] = 'thisisasecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myapp.db'
+
+# other configuration values...
+app.config['CSRF_ENABLED'] = True
+app.config['USER_ENABLE_EMAIL'] = False
+
+# create db for application ...
+db = SQLAlchemy(app)
+
+
+class User(db.Model, UserMixin):        # create table...
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False, server_default='')
+    active = db.Column(db.Boolean(), nullable=False, server_default='0')
+
+
+# set up the flask_user config object....
+db_adapter = SQLAlchemyAdapter(db, User)
+user_manager = UserManager(db_adapter, app)
+
+
+@app.route('/')
+def index():
+    return 'this is the homepage....'
+
+
+@app.route('/profile')
+@login_required  # only accessible to people who are logged in..
+def profile():
+    return 'this is the profile page... '
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
